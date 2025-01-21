cd57029b49bb7fdbe940a22367639c74ee34b7aa
Augusto Stoffel <augusto.stoffel@dida.do>
2025-01-21 16:07:10 +0100

    Version 0.3

--- smartextract/__init__.py
+++ smartextract/__init__.py
@@ -21,7 +21,7 @@ if TYPE_CHECKING:
 import httpx
 from pydantic import BaseModel, EmailStr, Field, JsonValue
 
-__version__ = "0.2"
+__version__ = "0.3"
 
 DEFAULT_BASE_URL = "https://api.smartextract.ai"
 DEFAULT_TIMEOUT = 600  # seconds
