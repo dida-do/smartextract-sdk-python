130673ead852670bf99711c3bfc3c9ae1c77e3bd
Augusto Stoffel <augusto.stoffel@dida.do>
2025-06-24 14:42:43 +0200

    Version 0.5

--- smartextract/__init__.py
+++ smartextract/__init__.py
@@ -28,7 +28,7 @@ if TYPE_CHECKING:
     from typing import Self  # For Python ≤ 3.10
 
 
-__version__ = "0.4"
+__version__ = "0.5"
 
 DEFAULT_BASE_URL = "https://api.smartextract.ai"
 DEFAULT_TIMEOUT = 600  # seconds
